61f65be83de6bb5d940ac5212c2edb6129be61bd
Bhandari007 <bmuskan007@gmail.com>
2021-11-09 11:50:35 +0545

    Made some design changes

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -19,39 +19,74 @@ class _QuizPageState extends State<QuizPage> {
   Widget build(BuildContext context) {
     return  SafeArea(
         child: Scaffold(
+          backgroundColor: Colors.white38,
             body:Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
-                  margin: const EdgeInsets.all(40),
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  height: MediaQuery.of(context).size.height * 0.8,
+                  margin: const EdgeInsets.only(left: 10,right: 10),
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.9,
                   decoration:  BoxDecoration(
-                    color: Colors.pink,
+                    gradient: const LinearGradient(
+                      begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Colors.blue,
+                          Colors.grey
+                        ]
+                    ),
                     borderRadius: BorderRadius.circular(14.0)
                   ),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children:  [
+                      Expanded(child: Container()),
+                      Text("Quiz",
+                        style: GoogleFonts.lato(
+                          fontSize: 80,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),),
+                      const SizedBox(height: 30,),
                       const CircleAvatar(
                         radius: 70,
                         backgroundImage: AssetImage("assets/images/question_mark.jpg",),
                         backgroundColor: Colors.white,
                       ),
-                      const SizedBox(height: 20,),
-                      Text("${quizMind.questionBank[0].questionText} ?",
-                        style: GoogleFonts.lato(
-                        fontSize: 25,
-                      ),),
                       const SizedBox(height: 30,),
+                      Container(
+                        margin: const EdgeInsets.only(left: 20,right: 20),
+                        alignment: Alignment.center,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(14.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.4),
+                              offset: const Offset(12.0, 12.0)
+                            )
+                          ]
+                        ),
+                        child: Text("\"${quizMind.questionBank[0].questionText}\"",
+                          style: GoogleFonts.lato(
+                          fontSize: 25,
+                            fontStyle: FontStyle.italic
+                        ),),
+                      ),
+                      const SizedBox(height: 50,),
                       InkWell(
                         child: Container(
                           alignment: Alignment.center,
                           width: MediaQuery.of(context).size.width - 100,
                           height: 50,
                           decoration: BoxDecoration(
-                            color: Colors.redAccent ,
+                            gradient: const LinearGradient(colors: [
+                              Colors.deepOrange,
+                              Colors.white
+                            ]) ,
                             borderRadius: BorderRadius.circular(14.0)
                           ),
                           child: Text("True",style: GoogleFonts.lato(
@@ -79,7 +114,10 @@ class _QuizPageState extends State<QuizPage> {
                           width: MediaQuery.of(context).size.width - 100,
                           height: 50,
                           decoration: BoxDecoration(
-                              color:Colors.redAccent ,
+                              gradient: const LinearGradient(colors: [
+                                Colors.deepOrange,
+                                Colors.white,
+                              ]),
                               borderRadius: BorderRadius.circular(14.0)
                           ),
                           child: Text("False",style: GoogleFonts.lato(
@@ -100,10 +138,11 @@ class _QuizPageState extends State<QuizPage> {
                           });
                         },
                       ),
+                      Expanded(child: Container()),
                       const SizedBox(height: 20,),
                       Row(
                         children:scoreKeeper
-                      )
+                      ),
                     ],
                   ),
                 ),
